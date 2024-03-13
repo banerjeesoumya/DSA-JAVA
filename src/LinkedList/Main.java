f0b232ad719bd553124b88ad00753a53579e99bf
soumyarikbanerjee <126083869+banerjeesoumya@users.noreply.github.com>
2024-03-14 00:00:12 +0530

    Added the implementation and created objects for both CLL.java and DLL.java

--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -2,14 +2,37 @@ package LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        LL list = new LL();
-        list.insertFirst(3);
+//        LL list = new LL();
+//        list.insertFirst(3);
 //        list.insertFirst(4);
 //        list.insertFirst(5);
 //        list.insertLast(9);
-//        list.insert(2, 2);
+//        list.insert(2, 3);
+//        list.display();
+//        System.out.println("Deleted Element: " + list.delete(3));
+//        list.display();
+
+//        DLL list = new DLL();
+//        list.insertFirst(3);
+//        list.insertFirst(4);
+//        list.insertFirst(8);
+//        list.insertFirst(17);
+//        list.insertLast(99);
+//        list.insert(101, 99);
+//        list.display();
+//        list.deleteLast();
+//        list.display();
+
+        CLL list = new CLL();
+        list.insert(23);
+        list.insert(3);
+        list.insert(19);
+        list.insert(75);
+        list.insertFirst(12);
+        list.insertLast(99);
         list.display();
-        System.out.println("Deleted Element: " + list.deleteLast());
+        System.out.println("Deleted element " + list.delete(12));
+//        System.out.println("Deleted element " + list.deleteLast());
         list.display();
     }
 }
