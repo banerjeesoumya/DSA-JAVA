a3d96b53f3cf2d10eff1c749a66e9539f4060d08
soumyarikbanerjee <126083869+banerjeesoumya@users.noreply.github.com>
2024-05-14 10:24:17 +0530

    Just made some minor edits and passes some test cases

--- src/Arrays/Sorting/Bubble_Sort.java
+++ src/Arrays/Sorting/Bubble_Sort.java
@@ -4,26 +4,26 @@ import java.util.Arrays;
 
 public class Bubble_Sort {
     public static void main(String[] args) {
-        int[] arr = {5, 4, 3, 2, 1};
+        int[] arr = {24, 18, 38, 43, 14, 40, 1, 54};
         bubbleSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
     public static void bubbleSort(int[] arr) {
-        boolean swapped = false;
+//        boolean swapped = false;
         for (int i = 0; i < arr.length; i++) {
-            swapped = false;
-            for (int j = 0; j < arr.length - i - 1; j++) {
+//            swapped = false;
+            for (int j = 0; j < arr.length  - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
-                    swapped = true;
+//                    swapped = true;
                 }
             }
-            if (!swapped) {
-                break;
-            }
+//            if (!swapped) {
+//                break;
+//            }
         }
     }
 }
